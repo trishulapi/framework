49d5accc3b66911fd6bca112958dd7c51c8935b1
TrishulApi <trishulapiofficial@gmail.com>
2025-06-01 21:34:16 +0530

    initial commit

---
+++ SECURITY.md
@@ -0,0 +1,13 @@
+# Security Policy
+
+## Supported Versions
+
+
+| Version | Supported          |
+| ------- | ------------------ |
+| 1.x.x   | :white_check_mark: |
+
+
+## Reporting a Vulnerability
+
+If you see any kind of vulnerability in this project. You can go to discussions and report the issue.
---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+require_once "vendor/autoload.php";
+
+
+/* * This is the main entry point for the application.
+ * It initializes the application and handles the request.
+ * 
+ * Project Structure
+ * 
+ * src 
+ *  ├── app
+ *  │   ├── models
+ *  │   ├── repos
+ *  │   ├── services 
+ *  |   logs
+ *  │   ├── 2025-06-01_log.log
+ * index.php
+ * .env
+ * composer.json
+ * composer.lock
+ * 
+ * 
+ * * This file is responsible for bootstrapping the application.    
+ * 
+ * The TrishulApi\Core\App class is responsible for initializing the application.
+ * 
+ * $app = new \TrishulApi\Core\App();
+ * 
+ * Router::get("/",["hello"=>"world"]);
+ * 
+ * 
+ * 
+ * it has several useful and must have methods to configure the application.
+ * 
+ * 
+ * 
+ *
+ *  */
+
